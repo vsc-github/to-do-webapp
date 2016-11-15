feefe41914c24d25d3edec5ab9ad63dc0486c8c1
Vicious Nefarious <batty.wayn3@gmail.com>
2016-11-16 00:23:49 +0530

    signup post request working

--- config/passport.js
+++ config/passport.js
@@ -32,6 +32,7 @@ module.exports = function(passport) {
           newUser.save(function(err) {
             if (err)
               throw err;
+            console.log("New user created!");
             return done(null, newUser);
           });
         }
--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,15 @@ router.post('/login', passport.authenticate('local-login', {
   failureFlash: true,
 }));
 
+router.get('/reminders', isLoggedIn, function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ user: req.user }));
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+      return next();
+  res.redirect('/');
+}
+
 module.exports = router;
